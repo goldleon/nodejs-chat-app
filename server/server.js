b57a12d9cd292b0dad0cc49732f5c2561ca7cb3c
Anass Bouchtaoui <bouchtaoui.anass@outlook.com>
2018-02-14 09:00:28 +0000

    Add Socket IO to send and receive websocket signals

--- server/server.js
+++ server/server.js
@@ -1,16 +1,31 @@
+// Build in Modules
 const path = require("path");
+const http = require("http");
+
+// 3rd party Modules
 const express = require("express");
+const socketIO = require("socket.io");
 
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 
-app.listen(port, (err) => {
+io.on("connection", (socket) => {
+    console.log("New user Connected");
+    socket.on("disconnect" , () => {
+        console.log("User Disconnected from Server")
+    });
+});
+
+
+server.listen(port, (err) => {
     if(err){
         return console.log(err);
     } 
